a0b19f5e3e13fec1523260a78eca35651909db6d
Okibe Onmeje <Okibeogomola@gmail.com>
2023-12-25 18:10:34 +0100

    fix 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # installs a flask
+package {
+'Werkzeug':
+ensure   =>   '2.1.1',
+provider =>   'pip3'
+}
 
 package {
 'Flask':
 ensure   =>  '2.1.0',
-provider =>  'pip3'
+provider =>   'pip3',
+require  =>    Package['Werkzeug']
 }
