05b921663952a82eeb3c302e16f209fd6744ba2d
Okibe Onmeje <Okibeogomola@gmail.com>
2024-04-10 08:01:01 +0100

    add print None statement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,11 @@ def number_of_subscribers(subreddit):
     Get the number of subscribers from a subreddit
     '''
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {"User-Agent": "alx/1"}
+    header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/102.0.0.0 Safari/537.36'
+        }
     resp = requests.get(url, headers=header, allow_redirects=False)
     if resp.status_code == 200:
         json_resp = resp.json()
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,15 +9,15 @@ def top_ten(subreddit):
     '''
     Get the number of subscribers from a subreddit
     '''
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     header = {"User-Agent": "alx/1"}
     resp = requests.get(url, headers=header, allow_redirects=False)
     if resp.status_code == 200:
-        json_resp = resp.json()
-        count = 0
-        for data in json_resp['data']['children']:
-            if count < 10:
+        try:
+            json_resp = resp.json()
+            for data in json_resp['data']['children']:
                 print(data['data']['title'])
-                count = count + 1
+        except Exception as e:
+            print(None)
     else:
-        return None
+        print(None)
